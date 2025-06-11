ab71182e9c42d8d0402162f12a176932db6cb8c3
winverse <public.winverse@gmail.com>
2025-06-11 20:21:51 +0900

    feat: remove redundant packages and restructure project for scraper integration

---
+++ apps/scraper/test/app.spec.ts
@@ -0,0 +1,25 @@
+import { AppModule } from '@/app.module';
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { App } from 'supertest/types';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', async () => {
+    const result = await request(app.getHttpServer()).get('/');
+
+    expect(result.status).toBe(200);
+    expect(result.text).toBe('Hello World!');
+  });
+});
