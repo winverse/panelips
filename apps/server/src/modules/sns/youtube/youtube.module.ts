b47b28a192b84cc344521f94b708bab1fb853417
winverse <public.winverse@gmail.com>
2025-07-07 17:22:03 +0900

    feat: MongoDB 모듈 추가 및 YouTube 모듈에 연동

--- apps/server/src/modules/sns/youtube/youtube.module.ts
+++ apps/server/src/modules/sns/youtube/youtube.module.ts
@@ -1,10 +1,11 @@
 import { YoutubeRouter } from '@modules/sns/youtube/youtube.router.js';
 import { Module } from '@nestjs/common';
+import { MongoModule } from '@providers/mongo/index.js';
 import { TrpcModule } from '@src/trpc/trpc.module.js';
 import { YoutubeService } from './youtube.service.js';
 
 @Module({
-  imports: [TrpcModule],
+  imports: [TrpcModule, MongoModule],
   providers: [YoutubeService, YoutubeRouter],
   exports: [YoutubeService, YoutubeRouter],
 })
